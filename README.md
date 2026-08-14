75ee58ee10db70727a42cc63aef1752dbc346ec8
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2026-08-14 13:33:43 +0200

    🎉 Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# perturbation-modeling: Custom scripts of perturbation modeling
+
+## Contributing
+
+Please run `pre-commit install` and `gitmoji -i` on the CLI before starting to work on this repository!
---
+++ perturbation_modeling/__init__.py
@@ -0,0 +1,18 @@
+"""Custom scripts of perturbation modeling.
+
+Import the package::
+
+   import perturbation_modeling
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   example_function
+   ExampleClass
+"""
+
+__version__ = "0.0.1"  # denote a pre-release for 0.1.0 with 0.1rc1
+
+from ._core import ExampleClass, example_function
